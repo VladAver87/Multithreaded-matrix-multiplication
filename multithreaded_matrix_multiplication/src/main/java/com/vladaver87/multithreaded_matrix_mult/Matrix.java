387a571bb2c6f0c212636227a03c324703e49c71
Your NameVladAver87 <Averianov87@gmail.com>
2019-04-01 12:57:13 +0300

    Matrix class

--- multithreaded_matrix_multiplication/src/main/java/com/vladaver87/multithreaded_matrix_mult/Matrix.java
+++ multithreaded_matrix_multiplication/src/main/java/com/vladaver87/multithreaded_matrix_mult/Matrix.java
@@ -2,31 +2,36 @@ package com.vladaver87.multithreaded_matrix_mult;
 
 import java.util.Random;
 
-public class Matrix {
-	private int row;
-	private int col;
+public final class Matrix {
+	private final int row;
+	private final int col;
+	private final int[][]data;
 
-	public Matrix(int row, int col) {
+	private Matrix(int row, int col) {
 		this.row = row;
 		this.col = col;
+		data = new int[row][col];
+	}
+	
+	public static Matrix createNewMatrix(int row, int col) {
+		return new Matrix(row, col);
 	}
 
-	public int[][] fillNewMatrix() {
-		int[][] result = new int[row][col];
+	public Matrix fillNewMatrix() {
 		final Random random = new Random();
-
+		Matrix result = new Matrix(row, col);
 		for (int i = 0; i < row; i++) {
 			for (int j  = 0; j < col; j++) {
-				result[i][j] = random.nextInt(100);		
+				result.data[i][j] = random.nextInt(100);		
 			}
 		}
 		return result;
 	}
 
-	public void printMatrix(int[][] matrix) {
+	public void printMatrix(Matrix matrix) {
 		for (int i = 0; i < row; i++) {
 			for (int j  = 0; j < col; j++) {
-				System.out.print(" " + matrix[i][j]);
+				System.out.print(" " + matrix.data[i][j]);
 			}
 			System.out.println();
 		}
--- multithreaded_matrix_multiplication/src/main/java/com/vladaver87/multithreaded_matrix_mult/Test.java
+++ multithreaded_matrix_multiplication/src/main/java/com/vladaver87/multithreaded_matrix_mult/Test.java
@@ -3,15 +3,8 @@ package com.vladaver87.multithreaded_matrix_mult;
 public class Test {
 
 	public static void main(String[] args) {
-		Matrix m1 = new Matrix(2,2);
-		Matrix m2 = new Matrix(2,2);
-		Matrix result = new Matrix(2,2);
-		SimpleMatrixMultiply simpleMatrixMultiply = new SimpleMatrixMultiply();
+		Matrix m1 = Matrix.createNewMatrix(2, 2);
 		m1.printMatrix(m1.fillNewMatrix());
-		System.out.println("-----------------");
-		m2.printMatrix(m2.fillNewMatrix());
-		System.out.println("-----------------");
-		result.printMatrix(simpleMatrixMultiply.multiply(m1.fillNewMatrix(), m2.fillNewMatrix()));
 	}
 
 }
