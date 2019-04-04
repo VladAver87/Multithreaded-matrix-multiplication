dfc4626355e0f806a76c4db46920ea8ee53512e1
Your NameVladAver87 <Averianov87@gmail.com>
2019-04-04 14:45:45 +0300

    mult 1x1

--- multithreaded_matrix_multiplication/src/main/java/com/vladaver87/multithreaded_matrix_mult/ParallelMatrixMultiply.java
+++ multithreaded_matrix_multiplication/src/main/java/com/vladaver87/multithreaded_matrix_mult/ParallelMatrixMultiply.java
@@ -6,11 +6,9 @@ public class ParallelMatrixMultiply extends Thread implements IMultiply{
 	private final Matrix matrix2;
 	Matrix result;
 	private int startIndex;
-	private int endIndex;
 	
-	public ParallelMatrixMultiply(Matrix matrix1, Matrix matrix2, int startIndex, int endIndex) {
+	public ParallelMatrixMultiply(Matrix matrix1, Matrix matrix2, int startIndex) {
 		this.startIndex = startIndex;
-		this.endIndex = endIndex;
 		this.matrix1 = matrix1;
 		this.matrix2 = matrix2;
 		resultArr = new int[matrix1.getRow()][matrix2.getCol()];
@@ -21,24 +19,18 @@ public class ParallelMatrixMultiply extends Thread implements IMultiply{
 		int rowCount = matrix1.getRow();
 		int colCount = matrix2.getCol();
 		if (rowCount != colCount) throw new RuntimeException("Multiply is not avalable");
-		Thread[] threads = new Thread[matrix1.getRow() * matrix2.getCol()];
-		
-		for (int i = 0; i < threads.length; i++) {
-			threads[i] = new ParallelMatrixMultiply(matrix1, matrix2, startIndex++, endIndex++);
-			threads[i].start();
-		}
-		for (int i = 0; i < threads.length; i++) {
- 
-			threads[i].join();
-		}
+		Thread thread = new ParallelMatrixMultiply(matrix1, matrix2, startIndex);
+			thread.start();
+			thread.join();
+
 		return result;
 	}
 	
 	@Override
 	public void run() {
-		for (int i = startIndex; i < endIndex; i++) {
-			for (int j = startIndex; j < endIndex; j++) {
-				for (int k = startIndex; k < matrix1.getCol(); k++) {
+		for (int i = startIndex; i < matrix1.getRow(); i++) {
+			for (int j = startIndex; j < matrix2.getCol(); j++) {
+				for (int k = 0; k < matrix1.getCol(); k++) {
 					resultArr[i][j] += matrix1.getElement(i, k) * matrix2.getElement(k, j);
 
 				}
