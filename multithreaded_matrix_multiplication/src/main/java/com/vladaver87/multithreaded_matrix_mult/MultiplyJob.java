263817013449606d8e45a2dc5a2b97f80fd96a9a
Vlad Aver <Averianov87@gmail.com>
2019-04-05 09:53:50 +0300

    two of four elems

--- multithreaded_matrix_multiplication/src/main/java/com/vladaver87/multithreaded_matrix_mult/MultiplyJob.java
+++ multithreaded_matrix_multiplication/src/main/java/com/vladaver87/multithreaded_matrix_mult/MultiplyJob.java
@@ -1,13 +1,13 @@
 package com.vladaver87.multithreaded_matrix_mult;
 
-public class MultiplyJob implements Runnable{
+public class MultiplyJob implements Runnable {
 
 	private int[][] result;
 	private final Matrix matrix1;
 	private final Matrix matrix2;
 	private int firstIndex;
 	private int lastIndex;
-	
+
 	public MultiplyJob(Matrix matrix1, Matrix matrix2, int firstIndex, int lastIndex, int[][] result) {
 		this.result = result;
 		this.firstIndex = firstIndex;
@@ -16,17 +16,14 @@ public class MultiplyJob implements Runnable{
 		this.matrix2 = matrix2;
 	}
 
-	
 	private void sum(int i, int j) {
-			for (int k = 0; k < result.length; k++) {
-				result[i][j] += matrix1.getElement(i, k) * matrix2.getElement(k, j);				
-			}	
-	}
-	
-	public void run() {
-		for (int index = firstIndex; index < lastIndex; ++index) {
-			sum(index, index);
+		for (int k = 0; k < result.length; k++) {
+			result[i][j] += matrix1.getElement(i, k) * matrix2.getElement(k, j);
 		}
+	}
+
+	public void run() {
+		sum(firstIndex, lastIndex);
 	}
 
 }
--- multithreaded_matrix_multiplication/src/main/java/com/vladaver87/multithreaded_matrix_mult/ParallelMatrixMultiply.java
+++ multithreaded_matrix_multiplication/src/main/java/com/vladaver87/multithreaded_matrix_mult/ParallelMatrixMultiply.java
@@ -7,18 +7,17 @@ public class ParallelMatrixMultiply implements IMultiply{
 	public Matrix multiply(Matrix matrix1, Matrix matrix2) throws InterruptedException {
 		int rowCount = matrix1.getRow();
 		int colCount = matrix2.getCol();
-		 final int[][] result = new int[rowCount][colCount];
+		final int[][] result = new int[rowCount][colCount];
 		if (rowCount != colCount) throw new RuntimeException("Multiply is not avalable");
 		int threadCount = matrix1.getRow() * matrix2.getCol();
-		final int cellsForThread = (rowCount * colCount) / threadCount;
         int firstIndex = 0;
+        int lastIndex = 0;
 		Thread[] threads = new Thread[threadCount];
 			for (int i = 0; i < threads.length; i++) {
-				int lastIndex = firstIndex + cellsForThread;
-				threads[i] = new Thread(new MultiplyJob(matrix1, matrix2, firstIndex, lastIndex, result));
+				threads[i] = new Thread(new MultiplyJob(matrix1, matrix2, firstIndex, lastIndex , result));
 				threads[i].start();	
 				firstIndex = lastIndex;
-				
+				lastIndex++;
 			}
 			
 	        try {
